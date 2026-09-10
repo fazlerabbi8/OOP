cfdd97050ff5249c4600b888adcf519b7bfe6d90
fazlerabbi13 <mohammad0132794@gmail.com>
2026-09-10 18:59:50 +0600

    added quantity of Product type.

--- Class.js
+++ Class.js
@@ -2,10 +2,12 @@
 class Product {
     name;
     price;
+    quantity;
     inStock;
-    constructor(name, price) {
+    constructor(name, price, quantity) {
         this.name = name;
         this.price = price;
+        this.quantity = quantity;
         this.inStock = true;
     }
     sell() {
@@ -13,9 +15,10 @@ class Product {
         console.log(`${this.name} has been sold.`);
     }
 }
-const iphone = new Product("iPhone 17 Pro", 999);
-const macbook = new Product("MacBook Pro", 1999);
+const iphone = new Product("iPhone 17 Pro", 999, 10);
+const macbook = new Product("MacBook Pro", 1999, 20);
 iphone.sell();
-macbook.sell();
-console.log(macbook.inStock);
+// macbook.sell();
+// console.log(macbook.inStock);
 console.log(iphone.inStock);
+console.log(iphone.quantity);
--- Class.ts
+++ Class.ts
@@ -1,11 +1,13 @@
 class Product {
   name: string;
   price: number;
+  quantity: number;
   inStock: boolean;
 
-  constructor(name: string, price: number) {
+  constructor(name: string, price: number,  quantity: number) {
     this.name = name;
     this.price = price;
+    this.quantity = quantity;
     this.inStock = true;
   }
 
@@ -15,10 +17,11 @@ class Product {
   }
 }
 
-const iphone = new Product("iPhone 17 Pro", 999);
-const macbook = new Product("MacBook Pro", 1999);
+const iphone = new Product("iPhone 17 Pro", 999, 10);
+const macbook = new Product("MacBook Pro", 1999,20);
 
 iphone.sell(); 
-macbook.sell();
-console.log(macbook.inStock);
+// macbook.sell();
+// console.log(macbook.inStock);
 console.log(iphone.inStock);
+console.log(iphone.quantity);
